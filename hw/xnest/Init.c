0b878c2697913ac8ca67dfb56d9eef8d4e4a960f
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-03-10 10:04:54 +0000

    BeforeReset is now known as ddxBeforeReset

--- hw/xnest/Init.c
+++ hw/xnest/Init.c
@@ -150,10 +150,12 @@ void OsVendorFatalError()
     return;
 }
 
-void BeforeReset(void)
+#ifdef DDXBEFORERESET
+void ddxBeforeReset(void)
 {
     return;
 }
+#endif
 
 /* this is just to get the server to link on AIX */
 #ifdef AIXV3

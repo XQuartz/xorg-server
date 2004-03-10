b42154ca2570511a4cc5fdf3106a1409a663438e
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-03-10 14:47:46 +0000

    First 3 opengl-wrapper functions. The other will follow in a similar way

--- GL/windows/indirect.c
+++ GL/windows/indirect.c
@@ -1,5 +1,13 @@
-#include <GL/gl.h>
 
+#include <GL/gl.h>
+#include <glxserver.h>
+#include <glxext.h>
+
+#define WINDOWS_LEAN_AND_CLEAN
+//typedef int BOOL;
+//typedef unsigned short ATOM;
+//typedef unsigned char BYTE;
+#include <windows.h>
 /*
  * GLX implementation that uses Win32's OpenGL
  */
@@ -49,3 +57,45 @@ GLuint __glFloorLog2(GLuint val)
     }
     return c;
 }
+
+
+/*
+ * Wrapper functions
+ */
+
+#define RESOLVE(procname, symbol)                                       \
+    static Bool init = TRUE; \
+    static procname proc = NULL; \
+    if (init) { \
+        proc = (procname)wglGetProcAddress(symbol); \
+        init = FALSE; \
+    } \
+    if (proc == NULL) { \
+        __glXErrorCallBack(NULL, 0); \
+        return; \
+    } 
+        
+        
+
+
+GLAPI void GLAPIENTRY glGetColorTable( GLenum target, GLenum format,
+                                       GLenum type, GLvoid *table )
+{
+    RESOLVE(PFNGLGETCOLORTABLEPROC, "glGetColorTableEXT");
+    proc(target, format, type, table);
+}
+
+GLAPI void GLAPIENTRY glGetColorTableParameterfv( GLenum target, GLenum pname,
+                                                  GLfloat *params )
+{
+    RESOLVE(PFNGLGETCOLORTABLEPARAMETERFVPROC, "glGetColorTableParameterfvEXT");
+    proc(target, pname, params);
+}
+
+GLAPI void GLAPIENTRY glGetColorTableParameteriv( GLenum target, GLenum pname,
+                                                  GLint *params )
+{
+    RESOLVE(PFNGLGETCOLORTABLEPARAMETERIVPROC, "glGetColorTableParameterivEXT");
+    proc(target, pname, params);
+}
+

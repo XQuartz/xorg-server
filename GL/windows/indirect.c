96711d9f56a4268e06ffe3b9d4baadbd9efb871d
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-03-10 09:04:47 +0000

    Framework for native opengl use. It does not link since there are still     undefined references to opengl extenstions but the symbols use the     __stdcall attribute.

--- GL/windows/indirect.c
+++ GL/windows/indirect.c
@@ -1,3 +1,51 @@
+#include <GL/gl.h>
+
 /*
  * GLX implementation that uses Win32's OpenGL
  */
+
+/*
+ * Server-side GLX uses these functions which are normally defined
+ * in the OpenGL SI.
+ */
+
+GLint __glEvalComputeK(GLenum target)
+{
+    switch (target) {
+    case GL_MAP1_VERTEX_4:
+    case GL_MAP1_COLOR_4:
+    case GL_MAP1_TEXTURE_COORD_4:
+    case GL_MAP2_VERTEX_4:
+    case GL_MAP2_COLOR_4:
+    case GL_MAP2_TEXTURE_COORD_4:
+	return 4;
+    case GL_MAP1_VERTEX_3:
+    case GL_MAP1_TEXTURE_COORD_3:
+    case GL_MAP1_NORMAL:
+    case GL_MAP2_VERTEX_3:
+    case GL_MAP2_TEXTURE_COORD_3:
+    case GL_MAP2_NORMAL:
+	return 3;
+    case GL_MAP1_TEXTURE_COORD_2:
+    case GL_MAP2_TEXTURE_COORD_2:
+	return 2;
+    case GL_MAP1_TEXTURE_COORD_1:
+    case GL_MAP2_TEXTURE_COORD_1:
+    case GL_MAP1_INDEX:
+    case GL_MAP2_INDEX:
+	return 1;
+    default:
+	return 0;
+    }
+}
+
+GLuint __glFloorLog2(GLuint val)
+{
+    int c = 0;
+
+    while (val > 1) {
+	c++;
+	val >>= 1;
+    }
+    return c;
+}

35b447d96f39031babf85a59af185e2c2a907828
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-03-11 13:44:43 +0000

    moved all opengl wrappers to glwrap.c print an error if a called opengl     function can not be resolved

--- GL/windows/glwrap.c
+++ GL/windows/glwrap.c
@@ -5,63 +5,13 @@
 #include <glxext.h>
 
 #define WINDOWS_LEAN_AND_CLEAN
-//typedef int BOOL;
-//typedef unsigned short ATOM;
-//typedef unsigned char BYTE;
 #include <windows.h>
 /*
  * GLX implementation that uses Win32's OpenGL
  */
 
 /*
- * Server-side GLX uses these functions which are normally defined
- * in the OpenGL SI.
- */
-
-GLint __glEvalComputeK(GLenum target)
-{
-    switch (target) {
-    case GL_MAP1_VERTEX_4:
-    case GL_MAP1_COLOR_4:
-    case GL_MAP1_TEXTURE_COORD_4:
-    case GL_MAP2_VERTEX_4:
-    case GL_MAP2_COLOR_4:
-    case GL_MAP2_TEXTURE_COORD_4:
-	return 4;
-    case GL_MAP1_VERTEX_3:
-    case GL_MAP1_TEXTURE_COORD_3:
-    case GL_MAP1_NORMAL:
-    case GL_MAP2_VERTEX_3:
-    case GL_MAP2_TEXTURE_COORD_3:
-    case GL_MAP2_NORMAL:
-	return 3;
-    case GL_MAP1_TEXTURE_COORD_2:
-    case GL_MAP2_TEXTURE_COORD_2:
-	return 2;
-    case GL_MAP1_TEXTURE_COORD_1:
-    case GL_MAP2_TEXTURE_COORD_1:
-    case GL_MAP1_INDEX:
-    case GL_MAP2_INDEX:
-	return 1;
-    default:
-	return 0;
-    }
-}
-
-GLuint __glFloorLog2(GLuint val)
-{
-    int c = 0;
-
-    while (val > 1) {
-	c++;
-	val >>= 1;
-    }
-    return c;
-}
-
-
-/*
- * Wrapper functions
+ * Wrapper functions for Win32's OpenGL
  */
 
 #define RESOLVE_RET(procname, symbol, retval) \
@@ -70,6 +20,8 @@ GLuint __glFloorLog2(GLuint val)
     if (init) { \
         proc = (procname)wglGetProcAddress(symbol); \
         init = FALSE; \
+        if (proc == NULL) \
+            ErrorF("glwrap: Can't resolve \"%s\"\n", symbol); \
     } \
     if (proc == NULL) { \
         __glXErrorCallBack(NULL, 0); \

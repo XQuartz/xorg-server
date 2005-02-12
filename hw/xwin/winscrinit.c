8e2017cc91e6ff3105b3b2762d19c7c9beb4c139
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2005-02-12 14:29:22 +0000

    win.h winfont.c winmultiwindowshape.c winmultiwindowwindow.c winpfbdd.c winshaddd.c winshadddnl.c winshadgdi.c Fix incorrect wrapping of functions. Ensure the pointers from pScreen point     to the called function even if wrapped functions changed it Set the window properties to NULL to avoid referencing freed memory because     of timing problems after deleting a window Do not wrap ChangeWindowAttributes. All functions are noops currently

--- hw/xwin/winscrinit.c
+++ hw/xwin/winscrinit.c
@@ -475,7 +475,7 @@ winFinishScreenInitFB (int index,
       pScreen->CreateWindow = winCreateWindowRootless;
       pScreen->DestroyWindow = winDestroyWindowRootless;
       pScreen->PositionWindow = winPositionWindowRootless;
-      pScreen->ChangeWindowAttributes = winChangeWindowAttributesRootless;
+      /*pScreen->ChangeWindowAttributes = winChangeWindowAttributesRootless;*/
       pScreen->RealizeWindow = winMapWindowRootless;
       pScreen->UnrealizeWindow = winUnmapWindowRootless;
 #ifdef SHAPE
@@ -520,7 +520,7 @@ winFinishScreenInitFB (int index,
       pScreen->CreateWindow = winCreateWindowMultiWindow;
       pScreen->DestroyWindow = winDestroyWindowMultiWindow;
       pScreen->PositionWindow = winPositionWindowMultiWindow;
-      pScreen->ChangeWindowAttributes = winChangeWindowAttributesMultiWindow;
+      /*pScreen->ChangeWindowAttributes = winChangeWindowAttributesMultiWindow;*/
       pScreen->RealizeWindow = winMapWindowMultiWindow;
       pScreen->UnrealizeWindow = winUnmapWindowMultiWindow;
       pScreen->ReparentWindow = winReparentWindowMultiWindow;
@@ -685,7 +685,7 @@ winFinishScreenInitNativeGDI (int index,
   pScreen->CreateWindow = winCreateWindowNativeGDI;
   pScreen->DestroyWindow = winDestroyWindowNativeGDI;
   pScreen->PositionWindow = winPositionWindowNativeGDI;
-  pScreen->ChangeWindowAttributes = winChangeWindowAttributesNativeGDI;
+  /*pScreen->ChangeWindowAttributes = winChangeWindowAttributesNativeGDI;*/
   pScreen->RealizeWindow = winMapWindowNativeGDI;
   pScreen->UnrealizeWindow = winUnmapWindowNativeGDI;
 

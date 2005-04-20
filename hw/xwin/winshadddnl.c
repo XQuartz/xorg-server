f62afeb403b8b9243f83c4fbdcb9b1d11aa7d52d
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2005-04-20 09:37:50 +0000

    Print error code if winStoreColorsShadowDDNL fails

--- hw/xwin/winshadddnl.c
+++ hw/xwin/winshadddnl.c
@@ -1306,7 +1306,7 @@ winStoreColorsShadowDDNL (ColormapPtr pColormap,
 					  + pdefs[0].pixel);
   if (FAILED (ddrval))
     {
-      ErrorF ("winStoreColorsShadowDDNL - SetEntries () failed\n");
+      ErrorF ("winStoreColorsShadowDDNL - SetEntries () failed: %08x\n", ddrval);
       return FALSE;
     }
 

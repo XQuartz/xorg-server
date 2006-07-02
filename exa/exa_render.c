f8330f8d0fe1cdd4bca117537c7d1e918436ee3b
Eric Anholt <anholt@FreeBSD.org>
2006-07-02 12:41:35 +0200

    Fix bugs in support for new repeatTypes in XAA and EXA.
    EXA now won't pass pictures with new repeatTypes to drivers.  We can add a flag
    for them to support it at a later time.
    (cherry picked from 7106a77df37c06d2b5568eceeb9297096bff3137 commit)

--- exa/exa_render.c
+++ exa/exa_render.c
@@ -490,7 +490,8 @@ exaComposite(CARD8	op,
 	if (op == PictOpSrc)
 	{
 	    if (pSrc->pDrawable->width == 1 &&
-		pSrc->pDrawable->height == 1 && pSrc->repeat)
+		pSrc->pDrawable->height == 1 && pSrc->repeat &&
+		pSrc->repeatType == RepeatNormal)
 	    {
 		ret = exaTryDriverSolidFill(pSrc, pDst, xSrc, ySrc, xDst, yDst,
 					    width, height);
@@ -531,6 +532,8 @@ exaComposite(CARD8	op,
 
 
     if (pExaScr->info->PrepareComposite &&
+	(!pSrc->repeat || pSrc->repeat == RepeatNormal) &&
+	(!pMask || !pMask->repeat || pMask->repeat == RepeatNormal) &&
 	!pSrc->alphaMap && (!pMask || !pMask->alphaMap) && !pDst->alphaMap)
     {
 	ret = exaTryDriverComposite(op, pSrc, pMask, pDst, xSrc, ySrc, xMask,

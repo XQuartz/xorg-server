3a532addbd820e1f4420f811f27dfd9dd5b079d5
Ben Byer <bbyer@bbyer.apple.com>
2007-11-12 16:42:00 -0800

    converted some debugging prints to TRACE() statements

--- hw/darwin/quartz/quartz.h
+++ hw/darwin/quartz/quartz.h
@@ -38,6 +38,14 @@
 #include "screenint.h"
 #include "window.h"
 
+//#define DEBUG 1
+
+#ifdef DEBUG
+#define TRACE(msg, args...) ErrorF("%s:%d: " msg, __FUNCTION__, __LINE__, ##args )
+#else
+#define TRACE(msg, args...) 
+#endif
+
 /*------------------------------------------
    Quartz display mode function types
   ------------------------------------------*/

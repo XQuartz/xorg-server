841e715d6bf00661be62abb16723bab42d91131b
Ben Byer <bbyer@bbyer.local>
2007-11-16 06:02:54 -0800

    1.2a11

--- hw/darwin/apple/X11Application.m
+++ hw/darwin/apple/X11Application.m
@@ -160,7 +160,7 @@ message_kit_thread (SEL selector, NSObject *arg)
 	
     tem = [infoDict objectForKey:@"CFBundleShortVersionString"];
 	
-    [dict setObject:[NSString stringWithFormat:@"X11.app %@ - X.org X11R7.2 (xorg-server-1.2a10)",
+    [dict setObject:[NSString stringWithFormat:@"X11.app %@ - X.org X11R7.2 (xorg-server-1.2a11)",
 					 tem] forKey:@"ApplicationVersion"];
 	
     [self orderFrontStandardAboutPanelWithOptions: dict];

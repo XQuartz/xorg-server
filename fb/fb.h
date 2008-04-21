6cc9e4a0740fb58f60c97912b07b4bd04678d623
Jeremy Huddleston <jeremyhu@freedesktop.org>
2008-04-20 19:05:51 -0700

    fb: Removed some bounds checking debugging that we threw into fb a while back

--- fb/fb.h
+++ fb/fb.h
@@ -169,11 +169,6 @@ typedef CARD32		    FbStip;
 
 typedef int		    FbStride;
 
-#define CHECK_NULL(ptr) \
-  if ((ptr) == NULL) {\
-    ErrorF("%s:%d: null pointer\n", __FILE__, __LINE__); \
-    return; \
-  }
 
 #ifdef FB_DEBUG
 extern void fbValidateDrawable(DrawablePtr d);
@@ -718,7 +713,6 @@ typedef struct {
     (pointer) = (FbBits *) _pPix->devPrivate.ptr; \
     (stride) = ((int) _pPix->devKind) / sizeof (FbBits); (void)(stride); \
     (bpp) = _pPix->drawable.bitsPerPixel;  (void)(bpp); \
-    CHECK_NULL(pointer); \
 }
 
 #define fbGetStipDrawable(pDrawable, pointer, stride, bpp, xoff, yoff) { \
@@ -736,7 +730,6 @@ typedef struct {
     (pointer) = (FbStip *) _pPix->devPrivate.ptr; \
     (stride) = ((int) _pPix->devKind) / sizeof (FbStip); (void)(stride); \
     (bpp) = _pPix->drawable.bitsPerPixel; (void)(bpp); \
-    CHECK_NULL(pointer); \
 }
 
 /*

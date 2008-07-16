300322b883e2da11227516b9e81583044d137339
Adam Jackson <ajax@redhat.com>
2008-07-16 14:44:33 -0400

    Remove loadable font renderer support. (cherry picked from commit affec10635343668839994ea2bac16c1d4524200)

--- hw/xfree86/loader/xf86sym.c
+++ hw/xfree86/loader/xf86sym.c
@@ -706,7 +706,6 @@ _X_HIDDEN void *xfree86LookupTab[] = {
     SYMFUNC(LoaderErrorMsg)
     SYMFUNC(LoaderCheckUnresolved)
     SYMFUNC(LoadExtension)
-    SYMFUNC(LoadFont)
     SYMFUNC(LoaderReqSymbols)
     SYMFUNC(LoaderReqSymLists)
     SYMFUNC(LoaderRefSymbols)

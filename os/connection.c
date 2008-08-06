90f1024dc25acae1ade7d940002f75a4e459c96f
Jeremy Huddleston <jeremyhu@freedesktop.org>
2008-08-06 10:06:09 -0700

    Fixed bitwise or-and typo in my last commit. (cherry picked from commit 3ec234976079d30f10e5246f6847a7eee01c144e)

--- os/connection.c
+++ os/connection.c
@@ -707,7 +707,7 @@ ClientAuthorized(ClientPtr client,
     saddr = (struct sockaddr *) (trans_conn->addr);
     /* Allow any client to connect without authorization on a launchd socket,
        because it is securely created -- this prevents a race condition on launch */
-    if(trans_conn->flags | TRANS_NOXAUTH) {
+    if(trans_conn->flags & TRANS_NOXAUTH) {
         auth_id = (XID) 0L;
     } else {
         auth_id = CheckAuthorization (proto_n, auth_proto, string_n, auth_string, client, &reason);

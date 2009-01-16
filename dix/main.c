035c36ffa5da9d3c1bb98531977215e725684631
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-01-16 14:06:46 -0800

    XQuartz: mieq: Wait for the server to finish initializing before letting other threads mieqEnqueue
    Avoid possible race condition whereby one thread might call mieqEnqueue before InitAndStartDevices finishes
    (cherry picked from commit 94e417ac87a98cd5c6bf2d7c495d702748398931)

--- dix/main.c
+++ dix/main.c
@@ -242,6 +242,12 @@ static int indexForScanlinePad[ 65 ] = {
 #endif
 
 #ifdef XQUARTZ
+#include <pthread.h>
+
+BOOL serverInitComplete = FALSE;
+pthread_mutex_t serverInitCompleteMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t serverInitCompleteCond = PTHREAD_COND_INITIALIZER;
+
 int dix_main(int argc, char *argv[], char *envp[])
 #else
 int main(int argc, char *argv[], char *envp[])
@@ -444,6 +450,14 @@ int main(int argc, char *argv[], char *envp[])
 	    	FatalError("could not create connection block info");
 	    }
 	}
+
+#ifdef XQUARTZ
+    /* Let the other threads know the server is done with its init */
+    pthread_mutex_lock(&serverInitCompleteMutex);
+    serverInitComplete = TRUE;
+    pthread_cond_broadcast(&serverInitCompleteCond);
+    pthread_mutex_unlock(&serverInitCompleteMutex);
+#endif
 
 	Dispatch();
 

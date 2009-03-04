779e9fdc2de9bfebc5f0a4c9d5f42e389fdd8593
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-03-04 11:33:00 -0800

    XQuartz: Implement SendPSN for AppleWM

--- hw/xquartz/applewm.c
+++ hw/xquartz/applewm.c
@@ -505,6 +505,24 @@ ProcAppleWMSetWindowLevel(register ClientPtr client)
 }
 
 static int
+ProcAppleWMSendPSN(register ClientPtr client)
+{
+    REQUEST(xAppleWMSendPSNReq);
+    int err;
+    
+    REQUEST_SIZE_MATCH(xAppleWMSendPSNReq);
+    
+    if(appleWMProcs->SendPSN) {
+        err = appleWMProcs->SendPSN(stuff->psn_hi, stuff->psn_lo);
+        if (err != Success) {
+            return err;
+        }
+    }
+
+    return (client->noClientException);
+}
+
+static int
 ProcAppleWMSetCanQuit(
     register ClientPtr client
 )
@@ -664,6 +682,8 @@ ProcAppleWMDispatch (
         return ProcAppleWMFrameHitTest(client);
     case X_AppleWMFrameDraw:
         return ProcAppleWMFrameDraw(client);
+    case X_AppleWMSendPSN:
+        return ProcAppleWMSendPSN(client);
     default:
         return BadRequest;
     }

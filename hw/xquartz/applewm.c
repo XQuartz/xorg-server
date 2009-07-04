9c2e42d012baad20379bca26abd74c6867e68771
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-07-03 19:25:33 -0700

    XQuartz: ProcAppleWMAttachTransient to play nice with the new Dock in SL (cherry picked from commit ddc0242d8f291d0f961ba9aa8c883e39861ce2ad)

--- hw/xquartz/applewm.c
+++ hw/xquartz/applewm.c
@@ -514,6 +514,36 @@ ProcAppleWMSendPSN(register ClientPtr client)
 }
 
 static int
+ProcAppleWMAttachTransient(register ClientPtr client)
+{
+    WindowPtr pWinChild, pWinParent;
+    REQUEST(xAppleWMAttachTransientReq);
+    int err;
+    
+    REQUEST_SIZE_MATCH(xAppleWMAttachTransientReq);
+    
+    if(!appleWMProcs->AttachTransient)
+        return BadRequest;
+
+    if (Success != dixLookupWindow(&pWinChild, stuff->child, client, DixReadAccess))
+        return BadValue;
+
+    if(stuff->parent) {
+        if(Success != dixLookupWindow(&pWinParent, stuff->parent, client, DixReadAccess))
+            return BadValue;
+    } else {
+        pWinParent = NULL;
+    }
+
+    err = appleWMProcs->AttachTransient(pWinChild, pWinParent);
+    if (err != Success) {
+        return err;
+    }
+
+    return (client->noClientException);
+}
+
+static int
 ProcAppleWMSetCanQuit(
     register ClientPtr client
 )
@@ -675,6 +705,8 @@ ProcAppleWMDispatch (
         return ProcAppleWMFrameDraw(client);
     case X_AppleWMSendPSN:
         return ProcAppleWMSendPSN(client);
+    case X_AppleWMAttachTransient:
+        return ProcAppleWMAttachTransient(client);
     default:
         return BadRequest;
     }

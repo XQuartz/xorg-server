f7f4b9a3ad285b719207adb5fba4d93627e6939b
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-10-13 12:54:11 -0700

    Rootless: Abstract some of the Xplugin specific stuff which has crept into rootlessWindow.c
    The rootless extension now directly calls some Xplugin functions, and relies
    on types defined in Xplugin.h, which isn't very abstracted :-)
    
    This patch is a start at abstracting some of the Xplugin specific stuff which
    has crept into rootlessWindow.c.  This has been done in a pretty mindless fashion,
    without much thought as to if the additions to the generic rootless interface are
    the correct ones
    
    There is some confusion as to if RootlesscolormapCallback() returns a Bool or
    xp_error_enum value (not so abstact), but I have no way of checking, of finding
    out if Xplugin actually checks the result :-)
    
    Based on patches from Colin Harrison, Jon Turney and Yaakov Selkowitz
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>
    Signed-off-by: Jeremy Huddleston <jeremyhu@freedesktop.org>
    (cherry picked from commit 6980f77892e0409b44bd8f33ba82e7273c6462a4)

--- hw/xwin/winscrinit.c
+++ hw/xwin/winscrinit.c
@@ -59,8 +59,10 @@ winMWExtWMProcs = {
   winMWExtWMDamageRects,
 #endif
   winMWExtWMRootlessSwitchWindow,
-  NULL,//winWMExtWMDoReorderWindow,
-  
+  NULL,//winMWExtWMDoReorderWindow,
+  NULL,//winMWExtWMHideWindow,
+  NULL,//winMWExtWMUpdateColorMap,
+
   NULL,//winMWExtWMCopyBytes,
   NULL,//winMWExtWMFillBytes,
   NULL,//winMWExtWMCompositePixels,

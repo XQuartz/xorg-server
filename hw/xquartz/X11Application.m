XQuartz/xorg-server

commit 55ef10975e2e543ff99f7a75dd65722dc96ff873

XQuartz: Add some sanity checking and a fallback for the bundle id.
This way we'll print an error and still mostly work rather than crashing if
someone installs XQuartz.app incorrectly or tries running the server within
the build system rather than the installed system.

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>
(cherry picked from commit 88ad050f3c2f9f11bafb56a8cf777518795295a9)

hw/xquartz/X11Application.m
@@ -992,6 +992,11 @@ void X11ApplicationMain (int argc, char **argv, char **envp) {
     
     app_prefs_domain_cfstr = (CFStringRef)[[NSBundle mainBundle] bundleIdentifier];
 
+    if (app_prefs_domain_cfstr == NULL) {
+        ErrorF("X11ApplicationMain: Unable to determine bundle identifier.  Your installation of XQuartz may be broken.\n");
+        app_prefs_domain_cfstr = @BUNDLE_ID_PREFIX".X11";
+    }
+
     [NSApp read_defaults];
     [NSBundle loadNibNamed:@"main" owner:NSApp];
     [[NSNotificationCenter defaultCenter] addObserver:NSApp

commit a0b8dacbc016e38c92300ed421850e11848b8454

XQuartz: Adjust BUNDLE_ID_PREFIX to LAUNCHD_ID_PREFIX for server-1.10-branch
Default builds will fail due to BUNDLE_ID_PREFIX not being defined.  LAUNCHD_ID_PREFIX
was used in 1.10.x and earlier.

This was fallout from 55ef10975e2e543ff99f7a75dd65722dc96ff873.

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

hw/xquartz/X11Application.m
@@ -1002,7 +1002,7 @@ void X11ApplicationMain (int argc, char **argv, char **envp) {
 
     if (app_prefs_domain_cfstr == NULL) {
         ErrorF("X11ApplicationMain: Unable to determine bundle identifier.  Your installation of XQuartz may be broken.\n");
-        app_prefs_domain_cfstr = @BUNDLE_ID_PREFIX".X11";
+        app_prefs_domain_cfstr = @LAUNCHD_ID_PREFIX".X11";
     }
 
     [NSApp read_defaults];
d8de110883532dcd7b9ca15d499503b00e357506
Kirill Elagin <kirelagin@gmail.com>
2011-10-04 23:02:20 +0400

    Fix server crash due to invalid images
    See https://bugs.freedesktop.org/show_bug.cgi?id=39383
    
    Signed-off-by: Kirill Elagin <kirelagin@gmail.com>
    Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>
    (cherry picked from commit 53611213396479abfdce0f7752681572e0d26d78)

--- fb/fbpict.c
+++ fb/fbpict.c
@@ -163,7 +163,9 @@ create_bits_picture (PicturePtr pict,
 	pict->format,
 	pixmap->drawable.width, pixmap->drawable.height,
 	(uint32_t *)bits, stride * sizeof (FbStride));
-    
+
+    if (!image)
+	return NULL;
     
 #ifdef FB_ACCESS_WRAPPER
 #if FB_SHIFT==5

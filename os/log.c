45b87d9c2e6d356a606170aefb7469df01f8e106
Jeremy Huddleston <jeremyhu@apple.com>
2012-03-28 15:07:51 -0700

    os: Fix regression with FatalError not calling va_start
    Regression From: a818b305989bbcde4e585112a7ee70cbc0b14a92
    
    Found-by: Colin Harrison <colin.harrison@virgin.net>
    Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>
    (cherry picked from commit 817cec8ffd6cdb1abbcc00decd43569d44801764)

--- os/log.c
+++ os/log.c
@@ -591,6 +591,8 @@ FatalError(const char *f, ...)
     else
         ErrorF("\nFatal server error:\n");
 
+    va_start(args, f);
+
     /* Make a copy for OsVendorFatalError */
     va_copy(args2, args);
 

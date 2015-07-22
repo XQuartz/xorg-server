f091528457cc62fa9bd6cd24aeebacffb7296419
Dave Airlie <airlied@redhat.com>
2015-07-22 12:14:06 -0400

    modesetting: Implement 32->24 bpp conversion in shadow update
    24bpp front buffers tend to be the least well tested path for client
    rendering.  On the qemu cirrus emulation, and on some Matrox G200 server
    chips, the hardware can't do 32bpp at all.  It's better to just allocate
    a 32bpp shadow and downconvert in the upload hook than expose a funky
    pixmap format to clients.
    
    [ajax: Ported from RHEL and separate modesetting driver, lifted kbpp
    into the drmmode struct, cleaned up commit message, fixed 16bpp]
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Dave Airlied <airlied@redhat.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    [hdegoede@redhat.com: rebase, also use kbpp for rotate shadow fb]
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    
    (cherry picked from commit 21217d02168d1883b2d1f64399aec494f96a8b9d)

--- hw/xfree86/drivers/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -408,7 +408,7 @@ drmmode_set_mode_major(xf86CrtcPtr crtc, DisplayModePtr mode,
         if (fb_id == 0) {
             ret = drmModeAddFB(drmmode->fd,
                                pScrn->virtualX, pScrn->virtualY,
-                               pScrn->depth, pScrn->bitsPerPixel,
+                               pScrn->depth, drmmode->kbpp,
                                drmmode_bo_get_pitch(&drmmode->front_bo),
                                drmmode_bo_get_handle(&drmmode->front_bo),
                                &drmmode->fb_id);
@@ -703,14 +703,14 @@ drmmode_shadow_allocate(xf86CrtcPtr crtc, int width, int height)
     int ret;
 
     if (!drmmode_create_bo(drmmode, &drmmode_crtc->rotate_bo,
-                           width, height, crtc->scrn->bitsPerPixel)) {
+                           width, height, drmmode->kbpp)) {
         xf86DrvMsg(crtc->scrn->scrnIndex, X_ERROR,
                "Couldn't allocate shadow memory for rotated CRTC\n");
         return NULL;
     }
 
     ret = drmModeAddFB(drmmode->fd, width, height, crtc->scrn->depth,
-                       crtc->scrn->bitsPerPixel,
+                       drmmode->kbpp,
                        drmmode_bo_get_pitch(&drmmode_crtc->rotate_bo),
                        drmmode_bo_get_handle(&drmmode_crtc->rotate_bo),
                        &drmmode_crtc->rotate_fb_id);
@@ -781,7 +781,7 @@ drmmode_shadow_create(xf86CrtcPtr crtc, void *data, int width, int height)
     rotate_pixmap = drmmode_create_pixmap_header(scrn->pScreen,
                                                  width, height,
                                                  scrn->depth,
-                                                 scrn->bitsPerPixel,
+                                                 drmmode->kbpp,
                                                  rotate_pitch,
                                                  pPixData);
 
@@ -1667,6 +1667,7 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
     uint32_t old_fb_id;
     int i, pitch, old_width, old_height, old_pitch;
     int cpp = (scrn->bitsPerPixel + 7) / 8;
+    int kcpp = (drmmode->kbpp + 7) / 8;
     PixmapPtr ppix = screen->GetScreenPixmap(screen);
     void *new_pixels = NULL;
 
@@ -1688,14 +1689,14 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
     old_front = drmmode->front_bo;
 
     if (!drmmode_create_bo(drmmode, &drmmode->front_bo,
-                           width, height, scrn->bitsPerPixel))
+                           width, height, drmmode->kbpp))
         goto fail;
 
     pitch = drmmode_bo_get_pitch(&drmmode->front_bo);
 
     scrn->virtualX = width;
     scrn->virtualY = height;
-    scrn->displayWidth = pitch / cpp;
+    scrn->displayWidth = pitch / kcpp;
 
     ret = drmModeAddFB(drmmode->fd, width, height, scrn->depth,
                        scrn->bitsPerPixel, pitch,
@@ -1711,8 +1712,7 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
     }
 
     if (drmmode->shadow_enable) {
-        uint32_t size = scrn->displayWidth * scrn->virtualY *
-            ((scrn->bitsPerPixel + 7) >> 3);
+        uint32_t size = scrn->displayWidth * scrn->virtualY * cpp;
         new_pixels = calloc(1, size);
         if (new_pixels == NULL)
             goto fail;
@@ -1720,7 +1720,8 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
         drmmode->shadow_fb = new_pixels;
     }
 
-    screen->ModifyPixmapHeader(ppix, width, height, -1, -1, pitch, new_pixels);
+    screen->ModifyPixmapHeader(ppix, width, height, -1, -1,
+                               scrn->displayWidth * cpp, new_pixels);
 
     if (!drmmode_glamor_handle_new_screen_pixmap(drmmode))
         goto fail;
@@ -1747,7 +1748,7 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
     drmmode->front_bo = old_front;
     scrn->virtualX = old_width;
     scrn->virtualY = old_height;
-    scrn->displayWidth = old_pitch / cpp;
+    scrn->displayWidth = old_pitch / kcpp;
     drmmode->fb_id = old_fb_id;
 
     return FALSE;
@@ -2103,7 +2104,7 @@ drmmode_create_initial_bos(ScrnInfoPtr pScrn, drmmode_ptr drmmode)
     xf86CrtcConfigPtr xf86_config = XF86_CRTC_CONFIG_PTR(pScrn);
     int width;
     int height;
-    int bpp = pScrn->bitsPerPixel;
+    int bpp = ms->drmmode.kbpp;
     int i;
     int cpp = (bpp + 7) / 8;
 

fd5203e94d0be993d22f8d989c09527a46b182db
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2015-12-26 10:56:52 -0800

    Revert "dix: Restore PaintWindow screen hook"
    This reverts commit cbd3cfbad3f07b20e90ea9498110f255813eb441.

--- mi/miscrinit.c
+++ mi/miscrinit.c
@@ -253,7 +253,6 @@ miScreenInit(ScreenPtr pScreen, void *pbits,  /* pointer to screen bits */
     pScreen->ClearToBackground = miClearToBackground;
     pScreen->ClipNotify = (ClipNotifyProcPtr) 0;
     pScreen->RestackWindow = (RestackWindowProcPtr) 0;
-    pScreen->PaintWindow = miPaintWindow;
     /* CreatePixmap, DestroyPixmap */
     /* RealizeFont, UnrealizeFont */
     /* CreateGC */

0dc271690b0841473e442ffbf97464876234a7c8
Eric Anholt <eric@anholt.net>
2019-03-26 13:33:09 -0700

    gitlab-ci: Don't bother building piglit.
    We don't use any of piglit's tests, just the framework.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- .gitlab-ci/debian-install.sh
+++ .gitlab-ci/debian-install.sh
@@ -6,8 +6,7 @@ set -o xtrace
 echo 'deb-src https://deb.debian.org/debian testing main' > /etc/apt/sources.list.d/deb-src.list
 apt-get update
 apt-get install -y \
-	meson git ca-certificates ccache cmake automake \
-	autoconf libtool libwaffle-dev \
+	meson git ca-certificates ccache automake autoconf libtool \
 	libxkbcommon-dev python3-mako python3-numpy python3-six \
 	x11-utils x11-xserver-utils xauth xvfb \
         libgl1 libglx-mesa0
@@ -15,13 +14,8 @@ apt-get install -y \
 apt-get build-dep -y xorg-server
 
 cd /root
+
 git clone https://gitlab.freedesktop.org/mesa/piglit.git
-cd piglit
-cmake -G Ninja -DPIGLIT_BUILD_GL_TESTS=OFF -DPIGLIT_BUILD_GLES1_TESTS=OFF \
-    -DPIGLIT_BUILD_GLES2_TESTS=OFF -DPIGLIT_BUILD_GLES3_TESTS=OFF \
-    -DPIGLIT_BUILD_DMA_BUF_TESTS=OFF -DPIGLIT_BUILD_GLX_TESTS=OFF
-ninja
-cd ..
 
 git clone https://gitlab.freedesktop.org/xorg/test/xts
 cd xts
@@ -36,6 +30,6 @@ echo 'path=/root/xts' >> piglit/piglit.conf
 
 find -name \*.a -o -name \*.o | xargs rm
 
-apt-get purge -y git cmake libwaffle-dev libxkbcommon-dev \
+apt-get purge -y git libxkbcommon-dev \
     x11-utils x11-xserver-utils xauth xvfb
 apt-get autoremove -y --purge

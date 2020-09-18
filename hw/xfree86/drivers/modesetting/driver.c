d03c0de77ba36bb5221d00ac84dcdd6aae0df453
Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
2020-09-18 14:29:25 +0200

    modesetting: Initialize present extension despite glamor is disabled
    Signed-off-by: Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -1933,10 +1933,6 @@ ScreenInit(ScreenPtr pScreen, int argc, char **argv)
                        "Failed to initialize the DRI2 extension.\n");
         }
 
-        if (!(ms->drmmode.present_enable = ms_present_screen_init(pScreen))) {
-            xf86DrvMsg(pScrn->scrnIndex, X_ERROR,
-                       "Failed to initialize the Present extension.\n");
-        }
         /* enable reverse prime if we are a GPU screen, and accelerated, and not
          * i915. i915 is happy scanning out from sysmem. */
         if (pScreen->isGPU) {
@@ -1955,6 +1951,11 @@ ScreenInit(ScreenPtr pScreen, int argc, char **argv)
         }
     }
 #endif
+    if (!(ms->drmmode.present_enable = ms_present_screen_init(pScreen))) {
+        xf86DrvMsg(pScrn->scrnIndex, X_ERROR,
+                   "Failed to initialize the Present extension.\n");
+    }
+
 
     pScrn->vtSema = TRUE;
 

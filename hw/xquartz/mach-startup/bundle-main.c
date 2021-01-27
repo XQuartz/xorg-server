11869110c5fb630be79f5c5196ac9550f99e16f3
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2021-01-27 13:33:22 -0800

    xquartz: Remove support for Lion and earlier versions of macOS
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- hw/xquartz/mach-startup/bundle-main.c
+++ hw/xquartz/mach-startup/bundle-main.c
@@ -57,8 +57,6 @@
 #include "mach_startup.h"
 #include "mach_startupServer.h"
 
-#include "console_redirect.h"
-
 /* From darwinEvents.c ... but don't want to pull in all the server cruft */
 void
 DarwinListenOnOpenFD(int fd);
@@ -520,24 +518,8 @@ setup_console_redirect(const char *bundle_id)
 
     asl_set_filter(aslc, ASL_FILTER_MASK_UPTO(ASL_LEVEL_WARNING));
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-# if MAC_OS_X_VERSION_MIN_REQUIRED < 1080
-    if (asl_log_descriptor)
-# endif
-    {
-        asl_log_descriptor(aslc, NULL, ASL_LEVEL_INFO, STDOUT_FILENO, ASL_LOG_DESCRIPTOR_WRITE);
-        asl_log_descriptor(aslc, NULL, ASL_LEVEL_NOTICE, STDERR_FILENO, ASL_LOG_DESCRIPTOR_WRITE);
-    }
-# if MAC_OS_X_VERSION_MIN_REQUIRED < 1080
-    else {
-        xq_asl_capture_fd(aslc, NULL, ASL_LEVEL_INFO, STDOUT_FILENO);
-        xq_asl_capture_fd(aslc, NULL, ASL_LEVEL_NOTICE, STDERR_FILENO);
-    }
-# endif
-#else
-    xq_asl_capture_fd(aslc, NULL, ASL_LEVEL_INFO, STDOUT_FILENO);
-    xq_asl_capture_fd(aslc, NULL, ASL_LEVEL_NOTICE, STDERR_FILENO);
-#endif
+    asl_log_descriptor(aslc, NULL, ASL_LEVEL_INFO, STDOUT_FILENO, ASL_LOG_DESCRIPTOR_WRITE);
+    asl_log_descriptor(aslc, NULL, ASL_LEVEL_NOTICE, STDERR_FILENO, ASL_LOG_DESCRIPTOR_WRITE);
 }
 
 static void

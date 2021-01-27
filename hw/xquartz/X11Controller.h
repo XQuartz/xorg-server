3307751aedb9d9cf1bbb5281e0297a6597da8b22
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2021-01-27 13:18:35 -0800

    xquartz: Remove support for Tiger and earlier versions of macOS
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>
    (cherry picked from commit 6e6db055f8b517ae9d63351d4c00fd480cb6b54a)

--- hw/xquartz/X11Controller.h
+++ hw/xquartz/X11Controller.h
@@ -46,16 +46,6 @@
 #undef BOOL
 #endif
 
-#ifndef NSINTEGER_DEFINED
-#if __LP64__ || NS_BUILD_32_LIKE_64
-typedef long NSInteger;
-typedef unsigned long NSUInteger;
-#else
-typedef int NSInteger;
-typedef unsigned int NSUInteger;
-#endif
-#endif
-
 @interface X11Controller : NSObject
 #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
     <NSTableViewDataSource>

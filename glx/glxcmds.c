XQuartz/xorg-server

commit c468d34c7208c9041f9c077b54a00ae9cccad6a3

glx: Set ContextTag for all contexts
Currently, xorgGlxMakeCurrent() would set the context tag only for
indirect GLX contexts.

However, several other places expect to find a context for the tag or
they would raise a GLXBadContextTag error, such as WaitGL() or WaitX().

Set the context tag for direct contexts as well, to avoid raising an
error and possibly killing the client.

Thanks to Erik Kurzinger <ekurzinger@nvidia.com> for spotting the issue.

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>

glx/glxcmds.c
@@ -664,8 +664,9 @@ xorgGlxMakeCurrent(ClientPtr client, GLXContextTag tag, XID drawId, XID readId,
         }
 
         glxc->currentClient = client;
-        glxServer.setContextTagPrivate(client, newContextTag, glxc);
     }
+
+    glxServer.setContextTagPrivate(client, newContextTag, glxc);
 
     if (prevglxc) {
         prevglxc->currentClient = NULL;

commit aad61e8e03311eb8bae4f7db59e65634733eadc2

glx: Fix use after free in MakeCurrent
The fix from commit c468d34c7 - "glx: Set ContextTag for all contexts"
is actually incomplete, it correctly sets the context tag for direct
contexts as well, but would fail to mark the context's currentClient.

As a result, when the context is destroyed, it would be freed
immediately rather than being just scheduled for deletion, even though
it is still current for some client. leading to a use-after-free.

Make sure to also set the context's currentClient for direct contexts as
well, not just indirect ones.

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Fixes: c468d34c7 - "glx: Set ContextTag for all contexts"
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1186
Reviewed-by: Adam Jackson <ajax@redhat.com>

glx/glxcmds.c
@@ -662,11 +662,11 @@ xorgGlxMakeCurrent(ClientPtr client, GLXContextTag tag, XID drawId, XID readId,
             glxc->readPriv = NULL;
             return __glXError(GLXBadContext);
         }
-
-        glxc->currentClient = client;
     }
 
     glxServer.setContextTagPrivate(client, newContextTag, glxc);
+    if (glxc)
+        glxc->currentClient = client;
 
     if (prevglxc) {
         prevglxc->currentClient = NULL;
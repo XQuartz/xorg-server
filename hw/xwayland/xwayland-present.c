XQuartz/xorg-server

commit b2a06e0700fa48c1e77fc687e6af39a4bb7c2ceb

xwayland/present: Drop sync_flip member of struct xwl_present_window
The same information can be determined from the flip queue.

Acked-by: Olivier Fourdan <ofourdan@redhat.com>

hw/xwayland/xwayland-present.c
@@ -122,7 +122,9 @@ xwl_present_get_pending_flip(struct xwl_present_window *xwl_present_window)
 static inline Bool
 xwl_present_has_pending_events(struct xwl_present_window *xwl_present_window)
 {
-    return !!xwl_present_window->sync_flip ||
+    present_vblank_ptr flip_pending = xwl_present_get_pending_flip(xwl_present_window);
+
+    return (flip_pending && flip_pending->sync_flip) ||
            !xorg_list_is_empty(&xwl_present_window->wait_list);
 }
 
@@ -442,17 +444,17 @@ xwl_present_buffer_release(void *data)
 static void
 xwl_present_msc_bump(struct xwl_present_window *xwl_present_window)
 {
+    present_vblank_ptr flip_pending = xwl_present_get_pending_flip(xwl_present_window);
     uint64_t msc = ++xwl_present_window->msc;
     struct xwl_present_event    *event, *tmp;
 
     xwl_present_window->ust = GetTimeInMicros();
 
-    event = xwl_present_window->sync_flip;
-    xwl_present_window->sync_flip = NULL;
-    if (event) {
+    if (flip_pending && flip_pending->sync_flip) {
+        event = xwl_present_event_from_id((uintptr_t)flip_pending);
         event->pending = FALSE;
 
-        xwl_present_flip_notify_vblank(&event->vblank, xwl_present_window->ust, msc);
+        xwl_present_flip_notify_vblank(flip_pending, xwl_present_window->ust, msc);
 
         if (!event->pixmap) {
             /* If the buffer was already released, clean up now */
@@ -757,8 +759,6 @@ xwl_present_flip(WindowPtr present_window,
     event->pending = TRUE;
 
     xorg_list_init(&event->list);
-    if (sync_flip)
-        xwl_present_window->sync_flip = event;
 
     xwl_pixmap_set_buffer_release_cb(pixmap, xwl_present_buffer_release, event);
 

hw/xwayland/xwayland-present.h
@@ -35,7 +35,6 @@
 
 #ifdef GLAMOR_HAS_GBM
 struct xwl_present_window {
-    struct xwl_present_event *sync_flip;
     WindowPtr window;
     struct xorg_list frame_callback_list;
 

commit 735dbde48294f9d0a8c3d791c44df14da9b91fc0

xwayland/present: Move xwl_present_reset_timer call out of xwl_present_flip
xwl_present_reset_timer checks if the pending flip is synchronous, so
we need to call it after adding the pending flip to the flip queue.

Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1219
Fixes: b2a06e0700fa "xwayland/present: Drop sync_flip member of struct xwl_present_window"
Tested-by: Olivier Fourdan <ofourdan@redhat.com>
Acked-by: Olivier Fourdan <ofourdan@redhat.com>

hw/xwayland/xwayland-present.c
@@ -689,9 +689,6 @@ xwl_present_flip(WindowPtr present_window,
                       &xwl_window->frame_callback_list);
     }
 
-    /* Realign timer */
-    xwl_present_reset_timer(xwl_present_window);
-
     xwl_surface_damage(xwl_window->xwl_screen, xwl_window->surface,
                        damage_box->x1 - present_window->drawable.x,
                        damage_box->y1 - present_window->drawable.y,
@@ -784,6 +781,10 @@ xwl_present_execute(present_vblank_ptr vblank, uint64_t ust, uint64_t crtc_msc)
 
                 /* Put pending flip at the flip queue head */
                 xorg_list_add(&vblank->event_queue, &xwl_present_window->flip_queue);
+
+                /* Realign timer */
+                xwl_present_reset_timer(xwl_present_window);
+
                 return;
             }
 
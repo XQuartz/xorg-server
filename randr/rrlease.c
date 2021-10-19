pelya/xserver-xsdl

commit e4e3447603b5fd3a38a92c3f972396d1f81168ad

Add RandR leases with modesetting driver support [v6]
This adds support for RandR CRTC/Output leases through the modesetting
driver, creating a lease using new kernel infrastructure and returning
that to a client through an fd which will have access to only those
resources.

v2:	Restore CRTC mode when leases terminate

	When a lease terminates for a crtc we have saved data for, go
	ahead and restore the saved mode.

v3:	Report RR_Rotate_0 rotations for leased crtcs.

	Ignore leased CRTCs when selecting screen size.

	Stop leasing encoders, the kernel doesn't do that anymore.

	Turn off crtc->enabled while leased so that modesetting
	ignores them.

	Check lease status before calling any driver mode functions

	When starting a lease, mark leased CRTCs as disabled and hide
	their cursors. Also, check to see if there are other
	non-leased CRTCs which are driving leased Outputs and mark
	them as disabled as well. Sometimes an application will lease
	an idle crtc instead of the one already associated with the
	leased output.

	When terminating a lease, reset any CRTCs which are driving
	outputs that are no longer leased so that they start working
	again.

	This required splitting the DIX level lease termination code
	into two pieces, one to remove the lease from the system
	(RRLeaseTerminated) and a new function that frees the lease
	data structure (RRLeaseFree).

v4:	Report RR_Rotate_0 rotation for leased crtcs.

v5: Terminate all leases on server reset.

	Leases hang around after the associated client exits so that
	the client doesn't need to occupy an X server client slot and
	consume a file descriptor once it has gotten the output
	resources necessary.

	Any leases still hanging around when the X server resets or
	shuts down need to be cleaned up by calling the kernel to
	terminate the lease and freeing any DIX structures.

	Note that we cannot simply use the existing
	drmmode_terminate_lease function on each lease as that wants

randr/rrlease.c
@@ -1,0 +1,351 @@
+/*
+ * Copyright © 2017 Keith Packard
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
+#include "randrstr.h"
+#include "swaprep.h"
+#include <unistd.h>
+
+RESTYPE RRLeaseType;
+
+/*
+ * Notify of some lease change
+ */
+void
+RRDeliverLeaseEvent(ClientPtr client, WindowPtr window)
+{
+    ScreenPtr screen = window->drawable.pScreen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+    RRLeasePtr lease;
+
+    UpdateCurrentTimeIf();
+    xorg_list_for_each_entry(lease, &scr_priv->leases, list) {
+        if (lease->id != None && (lease->state == RRLeaseCreating ||
+                                  lease->state == RRLeaseTerminating))
+        {
+            xRRLeaseNotifyEvent le = (xRRLeaseNotifyEvent) {
+                .type = RRNotify + RREventBase,
+                .subCode = RRNotify_Lease,
+                .timestamp = currentTime.milliseconds,
+                .window = window->drawable.id,
+                .lease = lease->id,
+                .created = lease->state == RRLeaseCreating,
+            };
+            WriteEventsToClient(client, 1, (xEvent *) &le);
+        }
+    }
+}
+
+/*
+ * Change the state of a lease and let anyone watching leases know
+ */
+static void
+RRLeaseChangeState(RRLeasePtr lease, RRLeaseState old, RRLeaseState new)
+{
+    ScreenPtr screen = lease->screen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+
+    lease->state = old;
+    scr_priv->leasesChanged = TRUE;
+    RRSetChanged(lease->screen);
+    RRTellChanged(lease->screen);
+    scr_priv->leasesChanged = FALSE;
+    lease->state = new;
+}
+
+/*
+ * Allocate and initialize a lease
+ */
+static RRLeasePtr
+RRLeaseAlloc(ScreenPtr screen, RRLease lid, int numCrtcs, int numOutputs)
+{
+    RRLeasePtr lease;
+    lease = calloc(1,
+                   sizeof(RRLeaseRec) +
+                   numCrtcs * sizeof (RRCrtcPtr) +
+                   numOutputs * sizeof(RROutputPtr));
+    if (!lease)
+        return NULL;
+    lease->screen = screen;
+    xorg_list_init(&lease->list);
+    lease->id = lid;
+    lease->state = RRLeaseCreating;
+    lease->numCrtcs = numCrtcs;
+    lease->numOutputs = numOutputs;
+    lease->crtcs = (RRCrtcPtr *) (lease + 1);
+    lease->outputs = (RROutputPtr *) (lease->crtcs + numCrtcs);
+    return lease;
+}
+
+/*
+ * Check if a crtc is leased
+ */
+Bool
+RRCrtcIsLeased(RRCrtcPtr crtc)
+{
+    ScreenPtr screen = crtc->pScreen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+    RRLeasePtr lease;
+    int c;
+
+    xorg_list_for_each_entry(lease, &scr_priv->leases, list) {
+        for (c = 0; c < lease->numCrtcs; c++)
+            if (lease->crtcs[c] == crtc)
+                return TRUE;
+    }
+    return FALSE;
+}
+
+/*
+ * Check if an output is leased
+ */
+Bool
+RROutputIsLeased(RROutputPtr output)
+{
+    ScreenPtr screen = output->pScreen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+    RRLeasePtr lease;
+    int o;
+
+    xorg_list_for_each_entry(lease, &scr_priv->leases, list) {
+        for (o = 0; o < lease->numOutputs; o++)
+            if (lease->outputs[o] == output)
+                return TRUE;
+    }
+    return FALSE;
+}
+
+/*
+ * A lease has been terminated.
+ * The driver is responsible for noticing and
+ * calling this function when that happens
+ */
+
+void
+RRLeaseTerminated(RRLeasePtr lease)
+{
+    /* Notify clients with events, but only if this isn't during lease creation */
+    if (lease->state == RRLeaseRunning)
+        RRLeaseChangeState(lease, RRLeaseTerminating, RRLeaseTerminating);
+
+    if (lease->id != None)
+        FreeResource(lease->id, RT_NONE);
+
+    xorg_list_del(&lease->list);
+}
+
+/*
+ * A lease is completely shut down and is
+ * ready to be deallocated
+ */
+
+void
+RRLeaseFree(RRLeasePtr lease)
+{
+    free(lease);
+}
+
+/*
+ * Ask the driver to terminate a lease. The
+ * driver will call RRLeaseTerminated when that has
+ * finished, which may be some time after this function returns
+ * if the driver operation is asynchronous
+ */
+static void
+RRTerminateLease(RRLeasePtr lease)
+{
+    ScreenPtr screen = lease->screen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+
+    scr_priv->rrTerminateLease(screen, lease);
+}
+
+/*
+ * Destroy a lease resource ID. All this
+ * does is note that the lease no longer has an ID, and
+ * so doesn't appear over the protocol anymore.
+ */
+static int
+RRLeaseDestroyResource(void *value, XID pid)
+{
+    RRLeasePtr lease = value;
+
+    lease->id = None;
+    return 1;
+}
+
+/*
+ * Create the lease resource type during server initialization
+ */
+Bool
+RRLeaseInit(void)
+{
+    RRLeaseType = CreateNewResourceType(RRLeaseDestroyResource, "LEASE");
+    if (!RRLeaseType)
+        return FALSE;
+    return TRUE;
+}
+
+int
+ProcRRCreateLease(ClientPtr client)
+{
+    REQUEST(xRRCreateLeaseReq);
+    xRRCreateLeaseReply rep;
+    WindowPtr window;
+    ScreenPtr screen;
+    rrScrPrivPtr scr_priv;
+    RRLeasePtr lease;
+    RRCrtc *crtcIds;
+    RROutput *outputIds;
+    int fd;
+    int rc;
+    unsigned long len;
+    int c, o;
+
+    REQUEST_AT_LEAST_SIZE(xRRCreateLeaseReq);
+
+    LEGAL_NEW_RESOURCE(stuff->lid, client);
+
+    rc = dixLookupWindow(&window, stuff->window, client, DixGetAttrAccess);
+    if (rc != Success)
+        return rc;
+
+    len = client->req_len - bytes_to_int32(sizeof(xRRCreateLeaseReq));
+
+    if (len != stuff->nCrtcs + stuff->nOutputs)
+        return BadLength;
+
+    screen = window->drawable.pScreen;
+    scr_priv = rrGetScrPriv(screen);
+
+    if (!scr_priv)
+        return BadMatch;
+
+    if (!scr_priv->rrCreateLease)
+        return BadMatch;
+
+    /* Allocate a structure to hold all of the lease information */
+
+    lease = RRLeaseAlloc(screen, stuff->lid, stuff->nCrtcs, stuff->nOutputs);
+    if (!lease)
+        return BadAlloc;
+
+    /* Look up all of the crtcs */
+    crtcIds = (RRCrtc *) (stuff + 1);
+    for (c = 0; c < stuff->nCrtcs; c++) {
+        RRCrtcPtr crtc;
+
+	rc = dixLookupResourceByType((void **)&crtc, crtcIds[c],
+                                     RRCrtcType, client, DixSetAttrAccess);
+
+        if (rc != Success) {
+            client->errorValue = crtcIds[c];
+            goto bail_lease;
+        }
+
+        if (RRCrtcIsLeased(crtc)) {
+            client->errorValue = crtcIds[c];
+            rc = BadAccess;
+            goto bail_lease;
+        }
+
+        lease->crtcs[c] = crtc;
+    }
+
+    /* Look up all of the outputs */
+    outputIds = (RROutput *) (crtcIds + stuff->nCrtcs);
+    for (o = 0; o < stuff->nOutputs; o++) {
+        RROutputPtr output;
+
+	rc = dixLookupResourceByType((void **)&output, outputIds[o],
+                                     RROutputType, client, DixSetAttrAccess);
+        if (rc != Success) {
+            client->errorValue = outputIds[o];
+            goto bail_lease;
+        }
+
+        if (RROutputIsLeased(output)) {
+            client->errorValue = outputIds[o];
+            rc = BadAccess;
+            goto bail_lease;
+        }
+
+        lease->outputs[o] = output;
+    }
+
+    rc = scr_priv->rrCreateLease(screen, lease, &fd);
+    if (rc != Success)
+        goto bail_lease;
+
+    xorg_list_add(&scr_priv->leases, &lease->list);
+
+    if (!AddResource(stuff->lid, RRLeaseType, lease)) {
+        close(fd);
+        return BadAlloc;
+    }
+
+    if (WriteFdToClient(client, fd, TRUE) < 0) {
+        RRTerminateLease(lease);
+        close(fd);
+        return BadAlloc;
+    }
+
+    RRLeaseChangeState(lease, RRLeaseCreating, RRLeaseRunning);
+
+    rep = (xRRCreateLeaseReply) {
+        .type = X_Reply,
+        .nfd = 1,
+        .sequenceNumber = client->sequence,
+        .length = 0,
+    };
+
+    if (client->swapped) {
+        swaps(&rep.sequenceNumber);
+        swapl(&rep.length);
+    }
+
+    WriteToClient(client, sizeof (rep), &rep);
+
+    return Success;
+
+bail_lease:
+    free(lease);
+    return rc;
+}
+
+int
+ProcRRFreeLease(ClientPtr client)
+{
+    REQUEST(xRRFreeLeaseReq);
+    RRLeasePtr lease;
+
+    REQUEST_SIZE_MATCH(xRRFreeLeaseReq);
+
+    VERIFY_RR_LEASE(stuff->lid, lease, DixDestroyAccess);
+
+    if (stuff->terminate)
+        RRTerminateLease(lease);
+    else
+        /* Get rid of the resource database entry */
+        FreeResource(stuff->lid, RT_NONE);
+
+    return Success;
+}

configure.ac
@@ -759,7 +759,7 @@ dnl List of libraries that require a specific version
 LIBAPPLEWM="applewm >= 1.4"
 LIBDMX="dmx >= 1.0.99.1"
 LIBDRI="dri >= 7.8.0"
-LIBDRM="libdrm >= 2.3.1"
+LIBDRM="libdrm >= 2.4.89"
 LIBEGL="egl"
 LIBGBM="gbm >= 10.2.0"
 LIBGL="gl >= 7.1.0"

hw/xfree86/drivers/modesetting/driver.c
@@ -1804,6 +1804,9 @@ CloseScreen(ScreenPtr pScreen)
         free(ms->drmmode.shadow_fb2);
         ms->drmmode.shadow_fb2 = NULL;
     }
+
+    drmmode_terminate_leases(pScrn, &ms->drmmode);
+
     drmmode_uevent_fini(pScrn, &ms->drmmode);
 
     drmmode_free_bos(pScrn, &ms->drmmode);

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -2085,8 +2085,152 @@ drmmode_xf86crtc_resize(ScrnInfoPtr scrn, int width, int height)
     return FALSE;
 }
 
+static void
+drmmode_validate_leases(ScrnInfoPtr scrn)
+{
+    ScreenPtr screen = scrn->pScreen;
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+    modesettingPtr ms = modesettingPTR(scrn);
+    drmmode_ptr drmmode = &ms->drmmode;
+    drmModeLesseeListPtr lessees;
+    RRLeasePtr lease, next;
+    int l;
+
+    /* We can't talk to the kernel about leases when VT switched */
+    if (!scrn->vtSema)
+        return;
+
+    lessees = drmModeListLessees(drmmode->fd);
+    if (!lessees)
+        return;
+
+    xorg_list_for_each_entry_safe(lease, next, &scr_priv->leases, list) {
+        drmmode_lease_private_ptr lease_private = lease->devPrivate;
+
+        for (l = 0; l < lessees->count; l++) {
+            if (lessees->lessees[l] == lease_private->lessee_id)
+                break;
+        }
+
+        /* check to see if the lease has gone away */
+        if (l == lessees->count) {
+            free(lease_private);
+            lease->devPrivate = NULL;
+            xf86CrtcLeaseTerminated(lease);
+        }
+    }
+
+    free(lessees);
+}
+
+static int
+drmmode_create_lease(RRLeasePtr lease, int *fd)
+{
+    ScreenPtr screen = lease->screen;
+    ScrnInfoPtr scrn = xf86ScreenToScrn(screen);
+    modesettingPtr ms = modesettingPTR(scrn);
+    drmmode_ptr drmmode = &ms->drmmode;
+    int ncrtc = lease->numCrtcs;
+    int noutput = lease->numOutputs;
+    int nobjects;
+    int c, o;
+    int i;
+    int lease_fd;
+    uint32_t *objects;
+    drmmode_lease_private_ptr   lease_private;
+
+    nobjects = ncrtc + noutput;
+
+    if (nobjects == 0)
+        return BadValue;
+
+    lease_private = calloc(1, sizeof (drmmode_lease_private_rec));
+    if (!lease_private)
+        return BadAlloc;
+
+    objects = xallocarray(nobjects, sizeof (uint32_t));
+
+    if (!objects) {
+        free(lease_private);
+        return BadAlloc;
+    }
+
+    i = 0;
+
+    /* Add CRTC ids */
+    for (c = 0; c < ncrtc; c++) {
+        xf86CrtcPtr crtc = lease->crtcs[c]->devPrivate;
+        drmmode_crtc_private_ptr drmmode_crtc = crtc->driver_private;
+
+        objects[i++] = drmmode_crtc->mode_crtc->crtc_id;
+    }
+
+    /* Add connector ids */
+
+    for (o = 0; o < noutput; o++) {
+        xf86OutputPtr   output = lease->outputs[o]->devPrivate;
+        drmmode_output_private_ptr drmmode_output = output->driver_private;
+
+        objects[i++] = drmmode_output->mode_output->connector_id;
+    }
+
+    /* call kernel to create lease */
+    assert (i == nobjects);
+
+    lease_fd = drmModeCreateLease(drmmode->fd, objects, nobjects, 0, &lease_private->lessee_id);
+
+    free(objects);
+
+    if (lease_fd < 0) {
+        free(lease_private);
+        return BadMatch;
+    }
+
+    lease->devPrivate = lease_private;
+
+    xf86CrtcLeaseStarted(lease);
+
+    *fd = lease_fd;
+    return Success;
+}
+
+static void
+drmmode_terminate_lease(RRLeasePtr lease)
+{
+    ScreenPtr screen = lease->screen;
+    ScrnInfoPtr scrn = xf86ScreenToScrn(screen);
+    modesettingPtr ms = modesettingPTR(scrn);
+    drmmode_ptr drmmode = &ms->drmmode;
+    drmmode_lease_private_ptr lease_private = lease->devPrivate;
+
+    if (drmModeRevokeLease(drmmode->fd, lease_private->lessee_id) == 0) {
+        free(lease_private);
+        lease->devPrivate = NULL;
+        xf86CrtcLeaseTerminated(lease);
+    }
+}
+
+void
+drmmode_terminate_leases(ScrnInfoPtr pScrn, drmmode_ptr drmmode)
+{
+    ScreenPtr screen = xf86ScrnToScreen(pScrn);
+    rrScrPrivPtr scr_priv = rrGetScrPriv(screen);
+    RRLeasePtr lease, next;
+
+    xorg_list_for_each_entry_safe(lease, next, &scr_priv->leases, list) {
+        drmmode_lease_private_ptr lease_private = lease->devPrivate;
+        drmModeRevokeLease(drmmode->fd, lease_private->lessee_id);
+        free(lease_private);
+        lease->devPrivate = NULL;
+        RRLeaseTerminated(lease);
+        RRLeaseFree(lease);
+    }
+}
+
 static const xf86CrtcConfigFuncsRec drmmode_xf86crtc_config_funcs = {
-    drmmode_xf86crtc_resize
+    .resize = drmmode_xf86crtc_resize,
+    .create_lease = drmmode_create_lease,
+    .terminate_lease = drmmode_terminate_lease
 };
 
 Bool
@@ -2222,6 +2366,10 @@ drmmode_set_desired_modes(ScrnInfoPtr pScrn, drmmode_ptr drmmode, Bool set_hw)
                 return FALSE;
         }
     }
+
+    /* Validate leases on VT re-entry */
+    drmmode_validate_leases(pScrn);
+
     return TRUE;
 }
 
@@ -2426,6 +2574,9 @@ drmmode_handle_uevents(int fd, void *closure)
         changed = TRUE;
         drmmode_output_init(scrn, drmmode, mode_res, i, TRUE, 0);
     }
+
+    /* Check to see if a lessee has disappeared */
+    drmmode_validate_leases(scrn);
 
     if (changed) {
         RRSetChanged(xf86ScrnToScreen(scrn));

hw/xfree86/drivers/modesetting/drmmode_display.h
@@ -143,6 +143,10 @@ typedef struct {
     int enc_clone_mask;
 } drmmode_output_private_rec, *drmmode_output_private_ptr;
 
+typedef struct {
+    uint32_t    lessee_id;
+} drmmode_lease_private_rec, *drmmode_lease_private_ptr;
+
 typedef struct _msPixmapPriv {
     uint32_t fb_id;
     struct dumb_bo *backing_bo; /* if this pixmap is backed by a dumb bo */
@@ -190,6 +194,8 @@ extern Bool drmmode_setup_colormap(ScreenPtr pScreen, ScrnInfoPtr pScrn);
 
 extern void drmmode_uevent_init(ScrnInfoPtr scrn, drmmode_ptr drmmode);
 extern void drmmode_uevent_fini(ScrnInfoPtr scrn, drmmode_ptr drmmode);
+
+extern void drmmode_terminate_leases(ScrnInfoPtr scrn, drmmode_ptr drmmode);
 
 Bool drmmode_create_initial_bos(ScrnInfoPtr pScrn, drmmode_ptr drmmode);
 void *drmmode_map_front_bo(drmmode_ptr drmmode);

hw/xfree86/modes/xf86Crtc.c
@@ -254,7 +254,7 @@ xf86CrtcSetModeTransform(xf86CrtcPtr crtc, DisplayModePtr mode,
     RRTransformRec saved_transform;
     Bool saved_transform_present;
 
-    crtc->enabled = xf86CrtcInUse(crtc);
+    crtc->enabled = xf86CrtcInUse(crtc) && !RRCrtcIsLeased(crtc->randr_crtc);;
 
     /* We only hit this if someone explicitly sends a "disabled" modeset. */
     if (!crtc->enabled) {
@@ -411,6 +411,10 @@ xf86CrtcSetOrigin(xf86CrtcPtr crtc, int x, int y)
 
     crtc->x = x;
     crtc->y = y;
+
+    if (RRCrtcIsLeased(crtc->randr_crtc))
+        return;
+
     if (crtc->funcs->set_origin) {
         if (!xf86CrtcRotate(crtc))
             return;
@@ -2652,6 +2656,9 @@ xf86InitialConfiguration(ScrnInfoPtr scrn, Bool canGrow)
 static void
 xf86DisableCrtc(xf86CrtcPtr crtc)
 {
+    if (RRCrtcIsLeased(crtc->randr_crtc))
+        return;
+
     crtc->funcs->dpms(crtc, DPMSModeOff);
     xf86_crtc_hide_cursor(crtc);
 }
@@ -2670,6 +2677,9 @@ xf86PrepareOutputs(ScrnInfoPtr scrn)
     for (o = 0; o < config->num_output; o++) {
         xf86OutputPtr output = config->output[o];
 
+        if (RROutputIsLeased(output->randr_output))
+            continue;
+
 #if RANDR_GET_CRTC_INTERFACE
         /* Disable outputs that are unused or will be re-routed */
         if (!output->funcs->get_crtc ||
@@ -2687,11 +2697,14 @@ xf86PrepareCrtcs(ScrnInfoPtr scrn)
     int c;
 
     for (c = 0; c < config->num_crtc; c++) {
-#if RANDR_GET_CRTC_INTERFACE
         xf86CrtcPtr crtc = config->crtc[c];
+#if RANDR_GET_CRTC_INTERFACE
         xf86OutputPtr output = NULL;
         uint32_t desired_outputs = 0, current_outputs = 0;
         int o;
+
+        if (RRCrtcIsLeased(crtc->randr_crtc))
+            continue;
 
         for (o = 0; o < config->num_output; o++) {
             output = config->output[o];
@@ -2713,6 +2726,9 @@ xf86PrepareCrtcs(ScrnInfoPtr scrn)
         if (desired_outputs != current_outputs || !desired_outputs)
             xf86DisableCrtc(crtc);
 #else
+        if (RRCrtcIsLeased(crtc->randr_crtc))
+            continue;
+
         xf86DisableCrtc(crtc);
 #endif
     }
@@ -2948,7 +2964,7 @@ xf86DPMSSet(ScrnInfoPtr scrn, int mode, int flags)
         for (i = 0; i < config->num_output; i++) {
             xf86OutputPtr output = config->output[i];
 
-            if (output->crtc != NULL)
+            if (!RROutputIsLeased(output->randr_output) && output->crtc != NULL)
                 (*output->funcs->dpms) (output, mode);
         }
     }
@@ -2964,7 +2980,7 @@ xf86DPMSSet(ScrnInfoPtr scrn, int mode, int flags)
         for (i = 0; i < config->num_output; i++) {
             xf86OutputPtr output = config->output[i];
 
-            if (output->crtc != NULL)
+            if (!RROutputIsLeased(output->randr_output) && output->crtc != NULL)
                 (*output->funcs->dpms) (output, mode);
         }
     }

hw/xfree86/modes/xf86Crtc.h
@@ -47,6 +47,7 @@
 
 typedef struct _xf86Crtc xf86CrtcRec, *xf86CrtcPtr;
 typedef struct _xf86Output xf86OutputRec, *xf86OutputPtr;
+typedef struct _xf86Lease xf86LeaseRec, *xf86LeasePtr;
 
 /* define a standard for connector types */
 typedef enum _xf86ConnectorType {
@@ -677,6 +678,54 @@ typedef struct _xf86ProviderFuncs {
 
 } xf86ProviderFuncsRec, *xf86ProviderFuncsPtr;
 
+#define XF86_LEASE_VERSION      1
+
+struct _xf86Lease {
+    /**
+     * ABI versioning
+     */
+    int version;
+
+    /**
+     * Associated ScrnInfo
+     */
+    ScrnInfoPtr scrn;
+
+    /**
+     * Driver private
+     */
+    void *driver_private;
+
+    /**
+     * RandR lease
+     */
+    RRLeasePtr randr_lease;
+
+    /*
+     * Contents of the lease
+     */
+
+    /**
+     * Number of leased CRTCs
+     */
+    int num_crtc;
+
+    /**
+     * Number of leased outputs
+     */
+    int num_output;
+
+    /**
+     * Array of pointers to leased CRTCs
+     */
+    RRCrtcPtr *crtcs;
+
+    /**
+     * Array of pointers to leased outputs
+     */
+    RROutputPtr *outputs;
+};
+
 typedef struct _xf86CrtcConfigFuncs {
     /**
      * Requests that the driver resize the screen.
@@ -692,7 +741,28 @@ typedef struct _xf86CrtcConfigFuncs {
      */
     Bool
      (*resize) (ScrnInfoPtr scrn, int width, int height);
+
+    /**
+     * Requests that the driver create a lease
+     */
+    int (*create_lease)(RRLeasePtr lease, int *fd);
+
+    /**
+     * Ask the driver to terminate a lease, freeing all
+     * driver resources
+     */
+    void (*terminate_lease)(RRLeasePtr lease);
 } xf86CrtcConfigFuncsRec, *xf86CrtcConfigFuncsPtr;
+
+/*
+ * The driver calls this when it detects that a lease
+ * has been terminated
+ */
+extern _X_EXPORT void
+xf86CrtcLeaseTerminated(RRLeasePtr lease);
+
+extern _X_EXPORT void
+xf86CrtcLeaseStarted(RRLeasePtr lease);
 
 typedef void (*xf86_crtc_notify_proc_ptr) (ScreenPtr pScreen);
 
@@ -1008,6 +1078,12 @@ extern _X_EXPORT Bool
  */
 extern _X_EXPORT void
 xf86_crtc_hide_cursor(xf86CrtcPtr crtc);
+
+/**
+ * Called by the driver to turn a single crtc's cursor on
+ */
+extern _X_EXPORT Bool
+xf86_crtc_show_cursor(xf86CrtcPtr crtc);
 
 /**
  * Called by the driver to turn cursors off

hw/xfree86/modes/xf86Cursors.c
@@ -344,11 +344,13 @@ xf86_hide_cursors(ScrnInfoPtr scrn)
     }
 }
 
-static Bool
+Bool
 xf86_crtc_show_cursor(xf86CrtcPtr crtc)
 {
-    if (!crtc->cursor_in_range)
+    if (!crtc->cursor_in_range) {
+        crtc->funcs->hide_cursor(crtc);
         return TRUE;
+    }
 
     if (!crtc->cursor_shown)
         crtc->cursor_shown = xf86_driver_show_cursor(crtc);

XQuartz/xorg-server

commit f467f85ca1f780d5c7cf3c20888e399708d761ac

Fix RandR leasing for more than 1 simultaneously active lease.
Due to a switched order of parameters in the xorg_list_add()
call inside ProcRRCreateLease(), adding a new lease for RandR
output leasing does not actually add the new RRLeasePtr lease
record to the list of existing leases for a X-Screen, but instead
replaces the existing list with a new list that has the new lease
as the only element, and probably leaks a bit of memory.

Therefore the server "forgets" all active leases for a screen,
except for the last added lease. If multiple leases are created
in a session, then destruction of all leases but the last one
will fail in many cases, e.g., during server shutdown in
RRCloseScreen(), or resource destruction, e.g., in
RRCrtcDestroyResource().

Most importantly, it fails if a client simply close(fd)'es the
DRM master descriptor to release a lease, quits, gets killed or
crashes. In this case the kernel will destroy the lease and shut
down the display output, then send a lease event via udev to the
ddx, which e.g., in the modesetting-ddx will trigger a call to
drmmode_validate_leases().

That function is supposed to detect the released lease and tell
the server to terminate the lease on the server side as well,
via xf86CrtcLeaseTerminated(), but this doesn't happen for all
the leases the server has forgotten. The end result is a dead
video output, as the server won't reinitialize the crtc's
corresponding to the terminated but forgotten lease.

This bug was observed when using the amdvlk AMD OSS Vulkan
driver and trying to lease multiple VKDisplay's, and also
under Mesa radv, as both Mesa Vulkan/WSI/Display and amdvlk
terminate leases by simply close()ing the lease fd, not by
sending explicit RandR protocol requests to free leases.

Leasing worked, but ending a session with multiple active
leases ended in a lot of unpleasant darkness.

Fixing the wrong argument order to xorg_list_add() fixes the
problem. Tested on single-X-Screen and dual-X-Screen setups,
with one, two or three active leases.

Please merge this for the upcoming server 21.1 branch.
Merging into server 1.20 would also make a lot of sense.

Fixes: e4e3447603b5fd3a38a92c3f972396d1f81168ad

randr/rrlease.c
@@ -295,7 +295,7 @@ ProcRRCreateLease(ClientPtr client)
     if (rc != Success)
         goto bail_lease;
 
-    xorg_list_add(&scr_priv->leases, &lease->list);
+    xorg_list_add(&lease->list, &scr_priv->leases);
 
     if (!AddResource(stuff->lid, RRLeaseType, lease)) {
         close(fd);
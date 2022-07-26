XQuartz/xorg-server

commit 7cdcdfea08ad9d63466286a6f94783cbf5fc3717

xwayland: Add -force-xrandr-emulation switch
Adds a -force-xrandr-emulation cmdline switch that always exposes extra
modes when viewporter isn't exposed by the Wayland compositor.

Having the additional modes exposed by the X server is important for
games to function and be configured

Compositors, such as Gamescope (the compositor for Steam Deck),
support only a single window that is rendered in the centre of the
screen that is scaled up to fill the screen by the compositor based
on some user scaling settings.

Exposing viewporter, wouldn't make sense here, and could mislead native
Wayland clients, so exposing dummy modesets in X is preferred here.

Signed-off-by: Joshua Ashton <joshua@froggi.es>
Reviewed-by: Olivier Fourdan <ofourdan@redhat.com>

hw/xwayland/xwayland.c
@@ -102,6 +102,7 @@ ddxUseMsg(void)
     ErrorF("-verbose [n]           verbose startup messages\n");
     ErrorF("-version               show the server version and exit\n");
     ErrorF("-noTouchPointerEmulation  disable touch pointer emulation\n");
+    ErrorF("-force-xrandr-emulation   force non-native modes to be exposed when viewporter is not exposed by the compositor\n");
 }
 
 static int init_fd = -1;

hw/xwayland/man/Xwayland.man
@@ -71,6 +71,10 @@ Disable touch pointer emulation. This allows the Wayland compositor to
 implement its own pointer emulation mechanism for X11 clients that don't
 support touch input.
 .TP 8
+.B \-force-xrandr-emulation
+Force additional non-native modes to be exposed when viewporter is not
+supported by the Wayland compositor.
+.TP 8
 .B \-rootless
 Run \fIXwayland\fP rootless, so that X clients integrate seamlessly with
 Wayland clients in a Wayland desktop. That requires the Wayland server

hw/xwayland/xwayland-output.c
@@ -355,7 +355,7 @@ output_get_rr_modes(struct xwl_output *xwl_output,
 
     *count = 1;
 
-    if (!xwl_screen_has_resolution_change_emulation(xwl_screen))
+    if (!xwl_screen_has_resolution_change_emulation(xwl_screen) && !xwl_screen->force_xrandr_emulation)
         return rr_modes;
 
     /* Add fake modes */

hw/xwayland/xwayland-screen.c
@@ -643,6 +643,9 @@ xwl_screen_init(ScreenPtr pScreen, int argc, char **argv)
             ErrorF("xwayland glamor: this build does not have EGLStream support\n");
 #endif
         }
+        else if (strcmp(argv[i], "-force-xrandr-emulation") == 0) {
+            xwl_screen->force_xrandr_emulation = 1;
+        }
     }
 
 #ifdef XWL_HAS_GLAMOR

hw/xwayland/xwayland-screen.h
@@ -56,6 +56,7 @@ struct xwl_screen {
     int rootless;
     int glamor;
     int present;
+    int force_xrandr_emulation;
 
     CreateScreenResourcesProcPtr CreateScreenResources;
     CloseScreenProcPtr CloseScreen;

hw/xwayland/xwayland.pc.in
@@ -12,3 +12,4 @@ have_listenfd=true
 have_verbose=true
 have_terminate_delay=true
 have_no_touch_pointer_emulation=true
+have_force_xrandr_emulation=true

commit 24d7d93ff2dc0ef4a0517734f0aae01cd7d57bba

xwayland: Fix "-force-xrandr-emulation"
Commit 7cdcdfea0 introduced a new command line option
"-force-xrandr-emulation", however it is missing from the
ddxProcessArgument().

As a result, trying to use that command option would result in a error:

(EE) Unrecognized option: -force-xrandr-emulation

Make sure "-force-xrandr-emulation" is accounted for in Xwayland's
ddxProcessArgument().

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Fixes: 7cdcdfea0 - xwayland: Add -force-xrandr-emulation switch

hw/xwayland/xwayland.c
@@ -230,6 +230,9 @@ ddxProcessArgument(int argc, char *argv[], int i)
         touchEmulatePointer = FALSE;
         return 1;
     }
+    else if (strcmp(argv[i], "-force-xrandr-emulation") == 0) {
+        return 1;
+    }
     else if (strcmp(argv[i], "-geometry") == 0) {
         CHECK_FOR_REQUIRED_ARGUMENTS(1);
         return 2;
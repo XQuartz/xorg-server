XQuartz/xorg-server

commit e2dcf31059a82ca41e95d41518710fccde19670a

CI: Update gitlab CI to use debian bullseye
Also install xkbcomp, libwayland-dev, and weston from debian since it is new enough now

Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

.gitlab-ci.yml
@@ -9,9 +9,9 @@
 # using the same tag.
 variables:
     FDO_UPSTREAM_REPO: xorg/xserver
-    FDO_DISTRIBUTION_VERSION: buster-slim
+    FDO_DISTRIBUTION_VERSION: bullseye-slim
     FDO_DISTRIBUTION_EXEC: 'env FDO_CI_CONCURRENT=${FDO_CI_CONCURRENT} bash .gitlab-ci/debian-install.sh'
-    FDO_DISTRIBUTION_TAG: "2022-05-04.1"
+    FDO_DISTRIBUTION_TAG: "2022-07-01"
 
 include:
   - project: 'freedesktop/ci-templates'
@@ -31,7 +31,7 @@ stages:
   # Cancel CI run if a newer commit is pushed to the same branch
   interruptible: true
 
-debian-buster:
+debian-bullseye:
   extends:
     - .fdo.container-build@debian
     - .ci-run-policy

.gitlab-ci/debian-install.sh
@@ -57,6 +57,7 @@ apt-get install -y \
 	libtool \
 	libudev-dev \
 	libunwind-dev \
+	libwayland-dev \
 	libx11-dev \
 	libx11-xcb-dev \
 	libxau-dev \
@@ -101,6 +102,7 @@ apt-get install -y \
 	python3-mako \
 	python3-numpy \
 	python3-six \
+	x11-xkb-utils \
 	xfonts-utils \
 	xkb-data \
 	xtrans-dev \
@@ -126,33 +128,13 @@ make -j${FDO_CI_CONCURRENT:-4} install
 popd
 rm -rf xorgproto
 
-# weston 9.0 requires libwayland >= 1.18
-git clone https://gitlab.freedesktop.org/wayland/wayland.git --depth 1 --branch=1.18.0
-cd wayland
-meson _build -D{documentation,dtd_validation}=false
-ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
-cd ..
-rm -rf wayland
-
-# Xwayland requires wayland-protocols >= 1.22, but Debian buster has 1.17 only
+# Xwayland requires wayland-protocols >= 1.22, but Debian bullseye has 1.20 only
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git --depth 1 --branch=1.22
 cd wayland-protocols
 ./autogen.sh
 make -j${FDO_CI_CONCURRENT:-4} install
 cd ..
 rm -rf wayland-protocols
-
-# Xwayland requires weston > 5.0, but Debian buster has 5.0 only
-git clone https://gitlab.freedesktop.org/wayland/weston.git --depth 1 --branch=9.0
-cd weston
-meson _build -Dbackend-{drm,drm-screencast-vaapi,fbdev,rdp,wayland,x11}=false \
-      -Dbackend-default=headless -Dcolor-management-{colord,lcms}=false \
-      -Ddemo-clients=false -Dimage-{jpeg,webp}=false \
-      -D{pipewire,remoting,screenshare,test-junit-xml,wcap-decode,weston-launch,xwayland}=false \
-      -Dshell-{fullscreen,ivi,kiosk}=false -Dsimple-clients=
-ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
-cd ..
-rm -rf weston
 
 # Install libdecor for Xwayland
 git clone https://gitlab.gnome.org/jadahl/libdecor.git --depth 1 --branch=0.1.0
@@ -185,14 +167,6 @@ echo 'path=/root/xts' >> piglit/piglit.conf
 
 find -name \*.a -o -name \*.o -o -name \*.c -o -name \*.h -o -name \*.la\* | xargs rm
 strip xts/xts5/*/.libs/*
-
-# Running meson dist requires xkbcomp 1.4.1 or newer, but Debian buster has 1.4.0 only
-git clone https://gitlab.freedesktop.org/xorg/app/xkbcomp.git --depth 1 --branch=xkbcomp-1.4.1
-cd xkbcomp
-./autogen.sh --datarootdir=/usr/share
-make -j${FDO_CI_CONCURRENT:-4} install
-cd ..
-rm -rf xkbcomp
 
 apt-get purge -y \
 	$EPHEMERAL

commit c637ca66eccfd2cd810d454d5592e4d93afacd8f

ci: Install weston from Debian
The commit below was meant to do this, but it slipped through the
cracks.

Fixes: e2dcf31059a8 ("CI: Update gitlab CI to use debian bullseye")

.gitlab-ci.yml
@@ -19,7 +19,7 @@ variables:
     FDO_UPSTREAM_REPO: xorg/xserver
     FDO_DISTRIBUTION_VERSION: bullseye-slim
     FDO_DISTRIBUTION_EXEC: 'env FDO_CI_CONCURRENT=${FDO_CI_CONCURRENT} bash .gitlab-ci/debian-install.sh'
-    FDO_DISTRIBUTION_TAG: "2022-08-11"
+    FDO_DISTRIBUTION_TAG: "2022-09-02-weston"
 
 include:
   - project: 'freedesktop/ci-templates'

.gitlab-ci/debian-install.sh
@@ -102,6 +102,7 @@ apt-get install -y \
 	python3-mako \
 	python3-numpy \
 	python3-six \
+	weston \
 	x11-xkb-utils \
 	xfonts-utils \
 	xkb-data \
XQuartz/xorg-server

commit 84897891e5e7eb44068cda22284dca70c6cfd1a2

ci: Install libxcvt from git
Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>

.gitlab-ci/debian-install.sh
@@ -107,6 +107,14 @@ apt-get install -y \
 
 cd /root
 
+# xserver requires libxcvt
+git clone https://gitlab.freedesktop.org/xorg/lib//libxcvt.git --depth 1 --branch=libxcvt-0.1.0
+cd libxcvt
+meson _build
+ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
+cd ..
+rm -rf libxcvt
+
 # xserver requires xorgproto >= 2021.4.99.2 for XI 2.3.99.1
 git clone https://gitlab.freedesktop.org/xorg/proto/xorgproto.git --depth 1 --branch=xorgproto-2021.4.99.2
 pushd xorgproto

.gitlab-ci.yml
@@ -11,7 +11,7 @@ variables:
     FDO_UPSTREAM_REPO: xorg/xserver
     FDO_DISTRIBUTION_VERSION: buster-slim
     FDO_DISTRIBUTION_EXEC: 'env FDO_CI_CONCURRENT=${FDO_CI_CONCURRENT} bash .gitlab-ci/debian-install.sh'
-    FDO_DISTRIBUTION_TAG: "2021-05-17"
+    FDO_DISTRIBUTION_TAG: "2021-07-09"
 
 include:
   - project: 'freedesktop/ci-templates'

commit 4603d64483bd16966da59fc9c3cf5a7a9591da0c

ci: remove redundant slash in libxcvt repository url
Fixes: 84897891e5e7 ("ci: Install libxcvt from git")
Signed-off-by: Luc Ma <luc@sietium.com>

.gitlab-ci/debian-install.sh
@@ -114,7 +114,7 @@ apt-get install -y \
 cd /root
 
 # xserver requires libxcvt
-git clone https://gitlab.freedesktop.org/xorg/lib//libxcvt.git --depth 1 --branch=libxcvt-0.1.0
+git clone https://gitlab.freedesktop.org/xorg/lib/libxcvt.git --depth 1 --branch=libxcvt-0.1.0
 cd libxcvt
 meson _build
 ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
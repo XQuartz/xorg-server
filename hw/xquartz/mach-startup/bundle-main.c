85e81dcc9aef41175d6f2eab25abdd1f12c0e328
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2022-11-27 22:23:43 -0800

    xquartz: Disable COMPOSITE at runtime
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- hw/xquartz/mach-startup/bundle-main.c
+++ hw/xquartz/mach-startup/bundle-main.c
@@ -72,6 +72,10 @@ FatalError(const char *f, ...) _X_ATTRIBUTE_PRINTF(1, 2) _X_NORETURN;
 
 extern int noPanoramiXExtension;
 
+#ifdef COMPOSITE
+extern Bool noCompositeExtension;
+#endif
+
 #define DEFAULT_CLIENT X11BINDIR "/xterm"
 #define DEFAULT_STARTX X11BINDIR "/startx -- " X11BINDIR "/Xquartz"
 #define DEFAULT_SHELL  "/bin/sh"
@@ -634,6 +638,11 @@ main(int argc, char **argv, char **envp)
 
     /* The server must not run the PanoramiX operations. */
     noPanoramiXExtension = TRUE;
+
+#ifdef COMPOSITE
+    /* https://gitlab.freedesktop.org/xorg/xserver/-/issues/1409 */
+    noCompositeExtension = TRUE;
+#endif
 
     /* Setup the initial crasherporter info */
     strlcpy(__crashreporter_info_buff__, __crashreporter_info__base,
